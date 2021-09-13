6c2f5bdf2877cdbab4e916572d31a8df2927a853
Dasalva <dasalvasuper@gmail.com>
2021-09-13 17:59:00 -0500

    configuracion de migraciones

--- database/migrations/2021_09_13_212324_create_schedules_table.php
+++ database/migrations/2021_09_13_212324_create_schedules_table.php
@@ -15,6 +15,8 @@ class CreateSchedulesTable extends Migration
     {
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
+            $table->time('Hora_inicio');
+            $table->time('Hora_final');
             $table->timestamps();
         });
     }
